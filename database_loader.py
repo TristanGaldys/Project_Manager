c2da58e3a5e25a092946d87d17a75ee8e11edada
pro70 <tristangaldys@gmail.com>
2023-08-13 16:52:25 -0400

    -Test Commit

--- database_loader.py
+++ database_loader.py
@@ -1,3 +1,5 @@
 """
 This is a file to load the database with User data for testing of the Project
+
+This data will be in a format that will be specified so we can fill the database from an outside file
 """
