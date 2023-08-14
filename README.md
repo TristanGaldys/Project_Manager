3dc3aada33d923697d230b4ec1bdbc7a4425e835
DraganGolic <105243599+dragangolic@users.noreply.github.com>
2023-08-14 10:53:09 -0400

    new fork

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # Project_Manager
 
-The goal of this project is for anyone to be able to open to Application and be able to select their Occupation and then be shown all the projects that are avaliable for the said Occupation
-Every task will have  a set of occupations that can work on it
+The goal of this project is to create an Application that will connect IT Professionals from all over the USA, who 
+has the same goal. To pay their bills, learn, help, or earn their position as experienced and job ready professionals. 
 
-We will be using SQL to manage all the data
+Anyone, who would be interested, to join and participate in team effort and work with different teams is welcome. 
+Develop a Software, Database, API, Web page or projects related to but not limited with Data Science. 
+
+Part 1: Introduction
+
+First step, select your occupation and choose or start any project that you are interested in. 
+Every task will have  a set of occupations that you can work on.
+
+Part 2: Tech
+
+We will be using SQLite3 to manage all the data.
+And our focus will be on Object-Oriented Programming
+
+Part 3: Goal
+
+During this Project we will consider any option that will allow us to build valuable product that will be recognized
+at the current market.  
--- database_loader.py
+++ database_loader.py
@@ -1,5 +1,70 @@
 """
 This is a file to load the database with User data for testing of the Project
-
-This data will be in a format that will be specified so we can fill the database from an outside file
 """
+
+import sqlite3
+
+conn = sqlite3.connect("project.db")
+cur = conn.cursor()
+
+# # Create Table USERS
+# cur.execute('''
+#     CREATE TABLE IF NOT EXISTS users(
+#     id INTEGER PRIMARY KEY,
+#     user TEXT,
+#     skill TEXT
+#     )
+#     ''')
+#
+# # Create Table PROJECTS
+# cur.execute('''
+#     CREATE TABLE IF NOT EXISTS projects (
+#         id INTEGER PRIMARY KEY,
+#         project TEXT,
+#         task TEXT
+#     )
+# ''')
+#
+#
+# # Adding data into users table
+# users_data = [
+#     ('Michael', 'Developer'),
+#     ('Tod', 'Data Analyst'),
+#     ('Maria', 'Data Engineer'),
+#     ('Tristan', 'Software Engineer'),
+#     ('Ana', 'Web Developer')
+# ]
+#
+# # Adding data into projects table
+# projects_data = [
+#     ('Game-Predator', 'Graphics'),
+#     ('Data Science', 'Machine Learning'),
+#     ('Data Base Management', 'RDBM'),
+#     ('Analytics-Finance', 'Scraping and Cleaning'),
+#     ('Mobile App', 'testing')
+# ]
+#
+# cur.executemany("INSERT INTO users (user, skill) VALUES (?, ?)", users_data)
+#
+# cur.executemany("INSERT INTO projects (project, task) VALUES (?, ?)", projects_data)
+#
+# print("Users and Project Table has been created.")
+
+# CHECK
+# Query and print data from 'users' table
+print("Users Table:")
+cur.execute("SELECT * FROM users")
+for row in cur.fetchall():
+    print(row)
+
+print("\n")
+
+# Query and print data from 'projects' table
+print("Projects Table:")
+cur.execute("SELECT * FROM projects")
+for row in cur.fetchall():
+    print(row)
+
+
+conn.commit()
+conn.close()
