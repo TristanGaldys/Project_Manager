63cf09aeee08cb0f389bd397256c7a9066aca3f5
DraganGolic <105243599+dragangolic@users.noreply.github.com>
2023-08-23 22:35:54 -0400

    Updated functions

--- database_loader.py
+++ database_loader.py
@@ -12,7 +12,8 @@ cur.execute('''
     CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY,
     name TEXT,
-    skill TEXT
+    password TEXT,
+    skills TEXT
     )
     ''')
 # Create Table PROJECTS
@@ -21,12 +22,12 @@ cur.execute('''
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         owner INTEGER,
         project TEXT,
-        project_id TEXT,
+        project_id INTEGER,
         tasks TEXT,
         start_date TEXT,
         description TEXT,
-        completed_tasks TEXT,
-        uncompleted_tasks TEXT
+        completed_tasks INTEGER,
+        uncompleted_tasks INTEGER
     )
 ''')
 # Create Table TASKS
@@ -44,99 +45,12 @@ cur.execute('''
 ''')
 
 # Adding data into users table
-users_data = [
-    ('John Brzenk', 'Data Science'),
-    ('Denis Cyplenkov', 'Data Analysis'),
-    ('Dave Chaffee', 'Data Analysis'),
-    ('Georgi Tsvetkov', 'Data Analysis'),
-    ('Levan Saginashvili', 'Back end software development'),
-    ('Artyom Morozov', 'Front end software development'),
-    ('David Dadikyan', 'Database'),
-    ('Ermes Gasparini', 'Database'),
-    ('Vitaly Laletin', 'Data Science'),
-    ('Jerry Cadorette', 'Data Science'),
-    ('Tobias Sporrong', 'Data Science'),
-    ('Devon Larratt', 'Data Science'),
-    ('Dmitry Silaev', 'Software Engineering'),
-    ('Evgeny Prudnik', 'Database'),
-    ('Eglė Vaitkutė', 'Database'),
-    ('Gabriela Vasconcelos', 'Data Analysis'),
-    ('Sarah Bäckman', 'Software Engineering'),
-    ('Barbora Bajciova', 'Database')
-]
+users_data =[]
 
 # Adding data into projects table
-projects_data = [
-    # ('id', 'owner', 'project', 'project_id', 'tasks', 'start date', 'description', 'completed tasks', 'uncompleted tasks')
-    ('1', 'Regression', '', 'Data Collection and Preparation', '09-00-2023', "Gather the necessary datasets, ensuring they're relevant and high-quality", '', ''),
-    ('3', 'Classification', '', 'Model Deployment and Testing', '09-00-2023', 'Deploy the trained model to a production environment or create a prediction pipeline', '', ''),
-    ('6', 'Clustering', '', 'Problem Definition ', '09-00-2023', "Define the problem you're trying to solve with regression", '', ''),
-    ('8', 'Sentiment Analysis', '', 'Documentation and Reporting', '09-10-2023', 'Document the entire process, including data preprocessing, feature engineering, model selection, and evaluation metrics', '', ''),
-    ('10', 'Recommender System', '', 'Model Evaluation', '09-12-2023', 'Evaluate models using appropriate evaluation metrics such as Mean Squared Error (MSE), Root Mean Squared Error (RMSE), Mean Absolute Error (MAE), R-squared', '', ''),
-    ('2', 'NLP', 'Model Training',  '', '09-13-2023', 'Train the selected models on the training dataset', '', ''),
-    ('4', 'Artificial Neural Network',  '', 'Model Selection', '09-12-2023', 'Choose appropriate regression algorithms based on the problem type (linear, polynomial, etc.) and dataset size', '', ''),
-    ('9', 'Electricity Bill Management System',  '', 'Requirement Gathering and Analysis', '08-22-2023', 'Understand the requirements of the management system, including user needs and business goals', '', ''),
-    ('7', 'Online Retail Application Database',  '', 'Database Design', '08-22-2023', 'Create an Entity-Relationship Diagram (ERD) to visualize the database structure', '', ''),
-    ('5', 'Inventory Control Management',  '', 'Schema Creation', '08-22-2023', 'Use a Database Management System (DBMS) to define tables, fields, data types, constraints, and relationships', '', ''),
-    ('20', 'Library Management System',  '', 'Data Population', '09-30-2023', 'Develop scripts or use tools to insert data into the tables', '', ''),
-    ('18', 'Student Database Management',  '', 'User Interface Design', '09-30-2023', 'Create mockups or wireframes to visualize the layout and functionalities', '', ''),
-    ('11', 'Payroll Management System',  '', 'Front-End Development', '09-01-2023', 'Integrate the front-end with the back-end to fetch and display data from the database', '', ''),
-    ('15', 'Voice-based Transport Enquiry System',  '', 'Database Connectivity', '09-01-2023', 'Establish a connection between the back-end server and the database', '', ''),
-    ('17', 'SMS-based Remote Server Monitoring System',  '', 'Authentication and Authorization', '09-01-2023', 'Implement user authentication to ensure secure access to the system', '', ''),
-    ('12', 'E-Learning Platform',  '', 'System Design', '09-01-2023', 'Architect the overall system, including database structure, backend services, and user interfaces', '', ''),
-    ('13', 'Advanced Employment Management System',  '', 'User Management', '09-01-2023', 'Implement user authentication and authorization mechanisms', '', ''),
-    ('19', 'Image Encryption using AES',  '', 'User Management', '09-01-2023', 'Manage roles and permissions for different user types ', '', ''),
-    ('14', 'Data Leak Detector',  '', 'Job Posting and Recruitment', '08-30-2023', 'Design a module for posting job vacancies', '', ''),
-    ('16', 'Face Detector AI-Based Model',  '', 'Performance Management', '08-30-2023', 'Develop mechanisms for setting employee goals and KPIs', '', ''),
-    ('40', 'Smart Health Prediction System',  '', 'Leave and Attendance Management', '08-30-2023', 'Design a system for employees to apply for leaves', '', ''),
-    ('31', 'Weather Forecasting Application',  '', 'Payroll and Compensation', '08-30-2023', 'Design a payroll module that calculates employee salaries based on attendance and other factors', '', ''),
-    ('37', 'E-Parking Challenge Generation System',  '', 'Reporting and Analytics', '08-30-2023', 'Allow custom queries and filters for generating specific reports', '', ''),
-    ('21', 'Portfolio Website',  '', 'UI/UX Design', '08-30-2023', 'Design the user interface elements, including buttons, forms, navigation, etc', '', ''),
-    ('23', 'To-Do App', 'Responsive Design',  '', '08-30-2023', 'Ensure the application is responsive and works well on various screen sizes (desktop, tablet, mobile)', '', ''),
-    ('27', 'Virtual Keyboard',  '', 'HTML/CSS Development', '08-30-2023', 'Write clean and semantic HTML code for the structure', '', ''),
-    ('33', 'Meme Generator',  '', 'Cross-Browser Compatibility', '08-27-2023', 'Test and ensure the application works correctly across different web browsers (Chrome, Firefox, Safari, Edge, etc.)', '', ''),
-    ('32', 'Spotify Clone',  '', 'Performance Optimization', '08-27-2023', 'Optimize images, scripts, and styles to improve page loading times', '', ''),
-    ('24', 'Chat App',  '', 'Code Review and Testing', '08-27-2023', 'Conduct code reviews to ensure code quality and consistency', '', ''),
-    ('34', 'E-Commerce Website',  '', 'Deployment', '08-27-2023', 'Deploy the front-end code to a web server or a content delivery network (CDN).', '', ''),
-    ('26', 'A User Authentication System. Image Source ',  '', 'Requirements Gathering and Analysis', '08-27-2023', "Understand the project's objectives and requirements", '', ''),
-    ('25', 'Build a Landing Page. Image Source',  '', 'Architecture and Design', '08-27-2023', 'Design the overall system architecture', '', ''),
-    ('39', 'Build Your Own Web Server',  '', 'Database Design and Development', '08-23-2023', 'Set up the database server and create tables', '', ''),
-    ('38', 'Build a Finance Application',  '', 'Server-Side Development', '08-23-2023', 'Develop backend APIs for communication with the frontend.', '', ''),
-    ('28', 'Create a Text Analyzer',  '', 'API Development', '08-23-2023', 'Design RESTful or GraphQL APIs for communication with clients', '', ''),
-    ('35', 'Build a To-Do App',  '', 'Middleware and Services', '08-23-2023', 'Integrate with third-party services like payment gateways or APIs', '', ''),
-    ('36', 'Build a CLI App',  '', 'Security', '08-23-2023', 'Implement encryption for data at rest and in transit', '', ''),
-    ('22', 'Web Scraping Internet Movie',  '', 'Defining the Problem and Objectives', '08-23-2023', 'Clearly define the goals and objectives of the data analysis project', '', ''),
-    ('26', 'Exploratory Data Analysis, Global suicide rates',  '', 'Data Collection and Acquisition', '08-22-2023', 'Identify the data sources that are relevant to your analysis', '', ''),
-    ('29', 'Exploratory Data Analysis, World Happiness Report',  '', 'Data Cleaning and Preprocessing', '08-22-2023', 'Handle missing data by imputing or removing it', '', ''),
-    ('30', 'Data visualization, Healthy food and sport',  '', 'Exploratory Data Analysis (EDA):', '08-22-2023', 'Perform data visualization to understand the distribution of variables', '', ''),
-    ('44', 'Data visualization, Covid-19',  '', 'Feature Engineering', '08-22-2023', 'Create new features that might provide more meaningful insights', '', ''),
-    ('41', 'Data visualization, Most followed on Instagram',  '', 'Model Building and Prediction ', '08-22-2023', 'Train and validate the models using appropriate techniques', '', ''),
-    ('43', 'Data cleaning, World Software Engineers db',  '', 'Interpretation and Insights', '08-22-2023', 'Address the initial questions or objectives set at the beginning of the project', '', ''),
-    ('42', 'Data cleaning, Banking data sets',  '', 'Documentation and Code Cleanup', '08-22-2023', 'Archive and store your code, datasets, and any other resources used in the project', '', ''),
-]
+projects_data = []
 
 # Adding data into tasks table
-# tasks_data = [
-#     # ('id', 'user_id', 'project_id', 'task', 'description', 'deadline', 'required skills'),
-#     ('', '1', '', '', '', '', ''),
-#     ('', '2', '', '', '', '', ''),
-#     ('', '3', '', '', '', '', ''),
-#     ('', '4', '', '', '', '', ''),
-#     ('', '5', '', '', '', '', ''),
-#     ('', '6', '', '', '', '', ''),
-#     ('', '7', '', '', '', '', ''),
-#     ('', '8', '', '', '', '', ''),
-#     ('', '9', '', '', '', '', ''),
-#     ('', '10', '', '', '', '', ''),
-#     ('', '11', '', '', '', '', ''),
-#     ('', '12', '', '', '', '', ''),
-#     ('', '13', '', '', '', '', ''),
-#     ('', '14', '', '', '', '', ''),
-#     ('', '15', '', '', '', '', ''),
-#     ('', '16', '', '', '', '', ''),
-#     ('', '17', '', '', '', '', ''),
-#     ('', '18', '', '', '', '', ''),
-# ]
 tasks_data = [
     # ('user_id', 'project_id', 'task', 'description', 'deadline', 'required skills'),
     ('1', '', '', '', None, ''),
@@ -160,9 +74,9 @@ tasks_data = [
 ]
 
 
-#cur.executemany("INSERT INTO users (name, skill) VALUES (?, ?)", users_data)
+cur.executemany("INSERT INTO users (name, skills) VALUES (?, ?)", users_data)
 
-#cur.executemany("INSERT INTO projects (owner, project, project_id, tasks, start_date, description, completed_tasks, uncompleted_tasks) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", projects_data)
+cur.executemany("INSERT INTO projects (owner, project, project_id, tasks, start_date, description, completed_tasks, uncompleted_tasks) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", projects_data)
 
 #cur.executemany("INSERT INTO tasks (user_id, project_id, task, description, deadline, required_skills) VALUES ( ?, ?, ?, ?, ?, ?)", tasks_data)
 
@@ -184,10 +98,14 @@ for row in cur.fetchall():
     print(row)
 
 print("\n")
+#
+# # Query and print data from 'tasks' table
+# print("Task Table:")
+# cur.execute("SELECT * FROM tasks")
+# for row in cur.fetchall():
+#     print(row)
 
-# Query and print data from 'tasks' table
-print("Task Table:")
-cur.execute("SELECT * FROM tasks")
+cur.execute("SELECT project from projects")
 for row in cur.fetchall():
     print(row)
 
--- query.py
+++ query.py
@@ -6,32 +6,85 @@ These function below are a few of the required functions for this application to
 import sqlite3
 
 conn = sqlite3.connect('project.db')
-cursor = conn.cursor()
-def verify_user(user):
+cur = conn.cursor()
+def verify_user(name, password=None):
     """
     This function will take in a String that is a User
-    Should Return true if the user exists and false if it doesnt
+    Should Return true if the user exists and false if it doesn't
     """
-    cursor.execute("SELECT EXISTS(SELECT 1 FROM users WHERE name = ?)", (user,))
-    result = cursor.fetchone()[0]
+    cur.execute("SELECT EXISTS(SELECT 1 FROM users WHERE name = ?)", (name,))
+    result = cur.fetchone()[0]
     return result == 1
 
-def create_user(user):
+def create_user(name,  password=None, skills=[]):
     """
     This function will take in a String that is a User
-    Should Return false if the user exists and true if it doesnt
+    Should Return false if the user exists and true if it doesn't
     """
-    if verify_user(user):
+    if verify_user(name):
         return False
-    cursor.execute("INSERT INTO users (name) VALUES (?)", (user,))
+    cur.execute("INSERT INTO users (name) VALUES (?)", (name,))
     conn.commit()
     return True
-def add_skill(user, skills):
+
+def get_available_projects():
+    """
+    Get a list of available projects from the projects table.
+    Returns a list of project names.
+    """
+    try:
+        cur.execute("SELECT project FROM projects")
+        projects = cur.fetchall()
+        return projects
+    except sqlite3.Error:
+        return []
+
+
+def add_skill(user, skill):
     """
     This function should add a skill or multiple to the user specified
     return true on success and false on failed
     """
-    pass
+    if not verify_user(user):
+        return False
+    developer_skills = [
+        "Python", "JavaScript", "Java", "C++", "C#", "HTML", "CSS", "SQL", "Ruby", "PHP", "Swift", "Kotlin",
+        "TypeScript",
+        "Go", "Rust", "Scala", "Perl", "R", "Shell Scripting", "Node.js", "React", "Angular", "Vue.js", "Django",
+        "Flask", "Spring Boot", "Ruby on Rails", "Laravel", "ASP.NET", "GraphQL", "RESTful API Development",
+        "WebSockets",
+        "WebAssembly", "Bootstrap", "Tailwind CSS", "Sass/SCSS", "Docker", "Kubernetes", "AWS", "Azure",
+        "Google Cloud Platform",
+        "Serverless Architecture", "Firebase", "MongoDB", "PostgreSQL", "MySQL", "SQLite", "Redis", "Elasticsearch",
+        "Git", "GitHub", "GitLab", "Bitbucket", "CI/CD", "Jenkins", "Travis CI", "Agile Development", "Scrum",
+        "Kanban", "Test-Driven Development (TDD)", "Behavior-Driven Development (BDD)", "Unit Testing",
+        "Integration Testing",
+        "End-to-End Testing", "Jest", "Mocha", "Chai", "Selenium", "Cypress", "Performance Optimization",
+        "Web Accessibility (WCAG)",
+        "SEO Best Practices", "Responsive Web Design", "Mobile App Development", "Progressive Web Apps (PWA)",
+        "WebVR/WebXR", "Three.js", "D3.js", "Game Development", "Unreal Engine", "Unity", "CryEngine",
+        "Machine Learning",
+        "TensorFlow", "PyTorch", "Natural Language Processing (NLP)", "Computer Vision", "Blockchain", "Ethereum",
+        "Smart Contracts", "Solidity", "Raspberry Pi", "Arduino", "IoT Development", "Embedded Systems Programming",
+        "Cybersecurity", "Penetration Testing", "Data Analysis", "Big Data", "Hadoop", "Spark", "Data Visualization",
+        "VR/AR Development"
+    ]
+    if isinstance(developer_skills, list):
+
+        try:
+            skills_str = ', '.join(skill)
+            cur.execute("UPDATE users SET skills =? WHERE name=?", (skills_str, user))
+            conn.commit()
+            return True
+        except sqlite3.Error as e:
+            conn.rollback()
+            print(f"Error adding skills for user '{user}': {e}")
+            return False
+    else:
+        print("invalid input: 'skill' parameter should be a list")
+        return False
+
+
 def find_tasks(user):
     """
     Uses the users name to find the tasks this person can work on based on their skills
@@ -44,12 +97,20 @@ def assign_task(user, task):
     return true on success and false on failed
     """
     pass
-def create_proj(project):
+def create_proj(user, project):
     """
     adds a empty project to the project table
     return true on success and false on failed
     """
-    pass
+    try:
+        cur.execute("INSERT INTO projects (owner, project) VALUES (?, ?)", (user, project,))
+        conn.commit()
+        # cur.execute("INSERT INTO projects SET project = ? WHERE owner = ?", (project, user))
+        # conn.commit()
+        return True
+    except sqlite3.Error as e:
+        conn.rollback()
+        return False
 def add_task(project, task, skill):
     """
     This will add a task to a project and link the skill that is required
@@ -118,23 +179,144 @@ def task_info(task):
     task_data = [""]
     return task_data
 
-user_to_verify = "John Doe"
-if verify_user(user_to_verify):
-    print(f"User '{user_to_verify}' exists.")
-else:
-    print(f"User '{user_to_verify}' does not exist.")
+user_to_verify = input("Please enter your name: ")
+user_exists = verify_user(user_to_verify)
 
-new_user = "Jane Smith"
-if create_user(new_user):
-    print(f"User '{new_user}' created successfully.")
-else:
-    print(f"User '{new_user}' already exists.")
+if user_exists:
+    print(f"Welcome, {user_to_verify}!")
+    projects = get_available_projects()
+    if projects:
+        print("Available projects:")
+        for index, project in enumerate(projects, start=0):
+            print(f"{index}. {project[0]}")
+    else:
+        new_user = user_to_verify
+        if create_user(new_user):
+            print(f"User '{new_user}' created successfully.")
+        else:
+            pass
 
-user = "John Doe"
-skills = ["Python", "Database Management"]
-if add_skill(user, skills):
-    print(f"Skills added for user '{user}'.")
-else:
-    print(f"Failed to add skills for user '{user}'.")
+developer_skills = [
+            "Python", "JavaScript", "Java", "C++", "C#", "HTML", "CSS", "SQL", "Ruby", "PHP", "Swift", "Kotlin",
+            "TypeScript",
+            "Go", "Rust", "Scala", "Perl", "R", "Shell Scripting", "Node.js", "React", "Angular", "Vue.js", "Django",
+            "Flask", "Spring Boot", "Ruby on Rails", "Laravel", "ASP.NET", "GraphQL", "RESTful API Development",
+            "WebSockets",
+            "WebAssembly", "Bootstrap", "Tailwind CSS", "Sass/SCSS", "Docker", "Kubernetes", "AWS", "Azure",
+            "Google Cloud Platform",
+            "Serverless Architecture", "Firebase", "MongoDB", "PostgreSQL", "MySQL", "SQLite", "Redis", "Elasticsearch",
+            "Git", "GitHub", "GitLab", "Bitbucket", "CI/CD", "Jenkins", "Travis CI", "Agile Development", "Scrum",
+            "Kanban", "Test-Driven Development (TDD)", "Behavior-Driven Development (BDD)", "Unit Testing",
+            "Integration Testing",
+            "End-to-End Testing", "Jest", "Mocha", "Chai", "Selenium", "Cypress", "Performance Optimization",
+            "Web Accessibility (WCAG)",
+            "SEO Best Practices", "Responsive Web Design", "Mobile App Development", "Progressive Web Apps (PWA)",
+            "WebVR/WebXR", "Three.js", "D3.js", "Game Development", "Unreal Engine", "Unity", "CryEngine",
+            "Machine Learning",
+            "TensorFlow", "PyTorch", "Natural Language Processing (NLP)", "Computer Vision", "Blockchain", "Ethereum",
+            "Smart Contracts", "Solidity", "Raspberry Pi", "Arduino", "IoT Development", "Embedded Systems Programming",
+            "Cybersecurity", "Penetration Testing", "Data Analysis", "Big Data", "Hadoop", "Spark", "Data Visualization",
+            "VR/AR Development"
+]
+if not user_exists:
+    new_user = user_to_verify
+    if create_user(new_user, password=None):
+        print(f"Welcome '{new_user}'.")
+    else:
+        print(f"New user '{new_user}' already exists.")
 
-# Continue calling other functions as needed
+    if add_skill(new_user, developer_skills):
+        selected_skills = []
+        print("Add all your skills from our list:")
+        for index, skill in enumerate(developer_skills, start=1):
+            print(f"{index}. {skill}")
+
+        selected_skill = None  # Initialize selected_skill outside the loop
+        while selected_skill != 'done':
+            selected_skill = input("Select a skill by its number (or 'done' to finish selecting): ")
+            if selected_skill.lower() == 'done':
+                break
+
+            try:
+                skill_index = int(selected_skill) - 1
+                if 0 <= skill_index < len(developer_skills):
+                    selected_skills.append(developer_skills[skill_index])
+                    print(f"Selected skill: {developer_skills[skill_index]}")
+                else:
+                    print("Invalid skill number. Please try again.")
+            except ValueError:
+                print("Invalid input. Please enter a valid skill number or 'done'.")
+
+        if selected_skills:
+            if add_skill(new_user, selected_skills):
+                pass
+            else:
+                print(f"Failed to add skills for user '{new_user}'.")
+    else:
+        print(f"Failed to add skills for user '{new_user}'.")
+
+project_choice = input("Do you want to continue with old project or start a new one? (old/new): ")
+if project_choice.lower() == "old":
+    proj_list = get_available_projects()
+    unique_project_names = list(set(project[0] for project in proj_list))
+    if unique_project_names:
+        print("Available projects:")
+        for index, project_name in enumerate(unique_project_names, start=1):
+            print(f"{index}. {project_name}")
+        selected_project_index = input("Select a project by its index number: ")
+        try:
+            selected_project_index = int(selected_project_index) - 1
+            if 0 <= selected_project_index < len(unique_project_names):
+                selected_project_name = unique_project_names[selected_project_index]
+                print(f"You selected project: {selected_project_name}")
+            else:
+                print("Invalid project index. Continuing with no project.")
+                selected_project_name = None
+        except ValueError:
+            print("Invalid input. Continuing with no project.")
+            selected_project_name = None
+    else:
+        print("No projects available to continue with.")
+        selected_project_name = None
+
+elif project_choice.lower() == "new":
+    new_project_name = input("Enter the name of the new project: ")
+    if create_proj(user_to_verify, new_project_name):
+        print(f"Project '{new_project_name}' created successfully.")
+    else:
+        print(f"Failed to create project '{new_project_name}'.")
+else:
+    print("No projects available at the moment. You can start a new Project.")
+    create_new_project = input("Would you like to create a new project? (yes/no): ")
+    if create_new_project.lower() == "yes":
+        new_project_name = input("Enter the name of the new project: ")
+        if create_proj(user_to_verify, new_project_name):
+            print(f"Project '{new_project_name}' created successfully.")
+        else:
+            print(f"Failed to create project '{new_project_name}'.")
+
+# developer_skills = [
+#             "Python", "JavaScript", "Java", "C++", "C#", "HTML", "CSS", "SQL", "Ruby", "PHP", "Swift", "Kotlin",
+#             "TypeScript",
+#             "Go", "Rust", "Scala", "Perl", "R", "Shell Scripting", "Node.js", "React", "Angular", "Vue.js", "Django",
+#             "Flask", "Spring Boot", "Ruby on Rails", "Laravel", "ASP.NET", "GraphQL", "RESTful API Development",
+#             "WebSockets",
+#             "WebAssembly", "Bootstrap", "Tailwind CSS", "Sass/SCSS", "Docker", "Kubernetes", "AWS", "Azure",
+#             "Google Cloud Platform",
+#             "Serverless Architecture", "Firebase", "MongoDB", "PostgreSQL", "MySQL", "SQLite", "Redis", "Elasticsearch",
+#             "Git", "GitHub", "GitLab", "Bitbucket", "CI/CD", "Jenkins", "Travis CI", "Agile Development", "Scrum",
+#             "Kanban", "Test-Driven Development (TDD)", "Behavior-Driven Development (BDD)", "Unit Testing",
+#             "Integration Testing",
+#             "End-to-End Testing", "Jest", "Mocha", "Chai", "Selenium", "Cypress", "Performance Optimization",
+#             "Web Accessibility (WCAG)",
+#             "SEO Best Practices", "Responsive Web Design", "Mobile App Development", "Progressive Web Apps (PWA)",
+#             "WebVR/WebXR", "Three.js", "D3.js", "Game Development", "Unreal Engine", "Unity", "CryEngine",
+#             "Machine Learning",
+#             "TensorFlow", "PyTorch", "Natural Language Processing (NLP)", "Computer Vision", "Blockchain", "Ethereum",
+#             "Smart Contracts", "Solidity", "Raspberry Pi", "Arduino", "IoT Development", "Embedded Systems Programming",
+#             "Cybersecurity", "Penetration Testing", "Data Analysis", "Big Data", "Hadoop", "Spark", "Data Visualization",
+#             "VR/AR Development"
+# ]
+
+
+
